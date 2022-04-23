145ef3d9627fa94d03c628bc03116d9e4e6a133c
redruin1 <tfschaefer222@gmail.com>
2022-04-22 22:06:55 -0400

    Update env.py

--- draftsman/env.py
+++ draftsman/env.py
@@ -988,8 +988,9 @@ def update(verbose=False):
     this routine.
     """
     # Figure out what directory we're in
-    env_dir = os.path.dirname(__file__)
+    # env_dir = os.path.dirname(__file__)
     # env_dir = draftsman.__path__[0]
+    env_dir = os.path.dirname(draftsman.__file__)
     # Create some quick access folders
     factorio_data = os.path.join(env_dir, "factorio-data")
     factorio_mods = os.path.join(env_dir, "factorio-mods")
