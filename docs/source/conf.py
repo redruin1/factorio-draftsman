de6249293a010020c7abcad5b89183dfa613011f
redruin1 <tfschaefer222@gmail.com>
2022-04-28 08:45:02 -0400

    added `setup` to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,17 +4,6 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
 # -- Project information -----------------------------------------------------
 
 project = 'factorio-draftsman'
@@ -79,3 +68,7 @@ html_static_path = ['_static']
 # ]
 
 highlight_language = 'python'
+
+def setup(app):
+    from draftsman.env import update
+    update(verbose=True)
